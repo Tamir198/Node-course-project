876cd0d43abf12d0f6b66cb7097ab1e6c2b7715c
Tamir <1tamir198@gmail.com>
2021-12-15 17:13:11 +0200

    Adding example of data share between request

--- routes/admin.js
+++ routes/admin.js
@@ -3,16 +3,22 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const { propfind } = require('./shop');
 
 const router = express.Router();
+const products = [];
 
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title});
   console.log(req.body);
   res.redirect('/');
 });
 
-module.exports = router;
+module.exports = {
+routs: router,
+products : products
+}
