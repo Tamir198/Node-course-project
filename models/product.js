ff8a876d9615ce70ae3c02773ad0be1a6e39f62d
Tamir <1tamir198@gmail.com>
2022-01-01 16:13:42 +0200

    Finish product module

--- models/product.js
+++ models/product.js
@@ -1,36 +1,39 @@
 const fs = require('fs');
 const path = require('path');
 
+const productsFilePath = path.join(
+  path.dirname(process.mainModule.filename),
+   'data', 'products.json'
+   );
+
 module.exports = class Product {
 
   constructor(productTitle) {
     this.productTitle = productTitle;
   }
-                                             
+                                    
   saveCurrentProduct(){
-    const productsFilePath = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-
     fs.readFile(productsFilePath, (err, fileContent) => {
       let products = [];
-      if (!err) {
-        if(!err && fileContent.length){
+        if(!err && fileContent.length > 0){
           products = JSON.parse(fileContent);
         }
-      }
+
       products.push(this);
+
       fs.writeFile(productsFilePath, JSON.stringify(products), (err) => {
         console.log(`Error writing to products file  ${err}`)
       })
     })
   };
 
-  static async getAllProducts(callBack){
-    const productsFilePath = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+  static async getAllProducts(renderProduct){
      fs.readFile(productsFilePath, (err, fileContent) => {
-      if (err) {
-        callBack([]);
+      if (err || fileContent.length < 1) {
+        renderProduct([]);
+        return;
       }
-      callBack(JSON.parse(fileContent));
+      renderProduct(JSON.parse(fileContent));
     });
   }
 };
